82c30de8de21d7169a1d132e3325f7cc9f5827e8
Martin Barrios <mar.barr1215@gmail.com>
2022-04-27 00:49:04 -0700

    Part 2

--- shooter.py
+++ shooter.py
@@ -8,30 +8,89 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+#framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#Define Player Action Variavbles
+moving_left = False
+moving_right = False
+
+#define colors
+BG = (144,201,120)
+
+def draw_bg():
+    screen.fill(BG)
+
+
+
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale ):
+    def __init__(self,char_type, x, y, scale, speed ):
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/player/Idle/0.png')
+        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width()* scale), int(img.get_height()* scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
-player = Soldier(200,200,3)
-player2 = Soldier(400,200,3)
+player = Soldier('player',200,200,3,5)
 
 
 #Keep Game Running
 run = True 
 while run:
-
+    
+    clock.tick(FPS)
+    draw_bg()
     player.draw()
-    player2.draw()
+    player.move(moving_left, moving_right)
 
+    
     for event in pygame.event.get():
+        #Quit Game
         if event.type == pygame.QUIT:
             run = False
+        #Keyboard input
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+        # Keyboard input key released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+        #Refresh Display
     pygame.display.update()
 pygame.quit()
